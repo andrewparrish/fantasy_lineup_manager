7bc87e4ef2d7c53f7e5fdb26d88c72bc6045ff24
Andrew Parrish <m.andrewparrish@gmail.com>
2018-04-07 18:15:35 -0400

    Better player position parsing

--- lib/fantasy_lineup_manager/bot.rb
+++ lib/fantasy_lineup_manager/bot.rb
@@ -10,18 +10,13 @@ module FantasyLineupManager
 
     def login(username, password)
       @bot.visit('http://www.espn.com/login')
+      wait_until_loaded("//*[@id='disneyid-iframe']")
       @bot.within_frame @bot.find(:xpath, "//*[@id='disneyid-iframe']") do
         @bot.find(:xpath, "//input[@type='email']").set username
         @bot.find(:xpath, "//input[@type='password']").set password
         @bot.find(:xpath, "//button[@type='submit']").click
       end
-
-      count = 0
-      until @bot.has_xpath?("//a[@id='global-user-trigger']") do
-        sleep(1)
-        count + 1
-        break if count > MAX_RETRY
-      end
+      wait_until_loaded("//a[@id='global-user-trigger']")
     end
 
     def get_team_links
@@ -33,6 +28,17 @@ module FantasyLineupManager
     def process_teams
       @bot.visit(@links.first + '&pnc=off')
       PlayerBot.new(@bot).process_players
+    end
+
+    private
+
+    def wait_until_loaded(xpath)
+      count = 0
+      until @bot.has_xpath?(xpath) do
+        sleep(1)
+        count + 1
+        break if count > MAX_RETRY
+      end
     end
   end
 end
--- lib/fantasy_lineup_manager/player.rb
+++ lib/fantasy_lineup_manager/player.rb
@@ -1,13 +1,22 @@
 module FantasyLineupManager
   class Player
-    attr_reader :stats
+    NAME_POSITION_REGEX = /(\D+),\s(\D{3,4})\s?((\s?[\w|,]+)+)[\s+|S*]?/
+
+    attr_reader :stats, :name
 
     def initialize(name:, position:, opponent:, game_status:, stats:)
-      @name = name
-      @position = position
+      self.name = name
+      @current_position = position
       @opponent = opponent
       @game_status = game_status
       @stats = stats
+    end
+
+    def name=(name_and_positions)
+      data = name_and_positions.match(NAME_POSITION_REGEX)
+      @team = data[2].strip
+      @name = data[1]
+      @positions = data[3].split(',').map(&:strip).map { |pos| pos.match(/(\w{1,2})\s?S?/)[1] }
     end
   end
 end
